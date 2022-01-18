b0a8afc3033a9316921ec8ca18bde2beeb9f72e3
Mateo Carreras <mateo.carreras@gmail.com>
2022-01-18 14:37:44 -0800

    A couple more tests of addresses and csv formatting

--- test/models/address_test.rb
+++ test/models/address_test.rb
@@ -5,4 +5,14 @@ class AddressTest < ActiveSupport::TestCase
     address = Address.new(line_one: "", line_two: "Suite 101", city: "Vancouver", country: "CANADA", region: "BC", postal_code: "A1B2C3")
     assert_not address.valid?
   end
+
+  test "address one liner with no line two" do
+    address = Address.new(line_one: "123 Main St.", line_two: "", city: "Somewhere", country: "USA", region: "CA", postal_code: "90210")
+    assert_equal "123 Main St., Somewhere, CA, 90210, USA", address.one_liner
+  end
+
+  test "address one liner with line two" do
+    address = Address.new(line_one: "123 Main St.", line_two: "Suite 101", city: "Somewhere", country: "USA", region: "CA", postal_code: "90210")
+    assert_equal "123 Main St., Suite 101, Somewhere, CA, 90210, USA", address.one_liner
+  end
 end
--- test/models/package_test.rb
+++ test/models/package_test.rb
@@ -9,9 +9,9 @@ class PackageTest < ActiveSupport::TestCase
 
   test "csv should be properly formatted" do
     package = Package.find(1)
-    address_one = Address.find(1)
-    address_two = Address.find(2)
-    expected = "1,Test Package,in_trasit,\"#{address_one.one_liner}\",\"#{address_two.one_liner}\"\n"
+    address_to = Address.find(1)
+    address_from = Address.find(2)
+    expected = "1,Test Package,in_transit,\"#{address_from.one_liner}\",\"#{address_to.one_liner}\"\n"
     assert_equal expected, package.to_csv
   end
 end
