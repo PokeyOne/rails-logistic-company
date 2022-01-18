77b21f2c510def0d5e738b1dd956fb894f83332a
Mateo Carreras <mateo.carreras@gmail.com>
2022-01-17 19:30:37 -0800

    package state validation and home page information

--- app/models/package.rb
+++ app/models/package.rb
@@ -1,5 +1,15 @@
 # The base unit of the business. This represents everything that is shipped.
 class Package < ApplicationRecord
+  STATES = %w(
+    information_recieved
+    in_transit
+    delivered
+    returned
+    cancelled
+  ).freeze
+
   belongs_to :from_address, class_name: "Address"
   belongs_to :to_address, class_name: "Address"
+
+  validates :state, presence: true, inclusion: { in: STATES }
 end
